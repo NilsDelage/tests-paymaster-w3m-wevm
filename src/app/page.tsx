07ab86fec551dacd3458d55be6b9d41ae2a33cc5
U <nils.del@protonmail.com>
2024-07-16 15:04:23 +0200

    quick fix: workaround to make the zksync's paymaster transactions work on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 "use client"
 import React, { useState } from 'react';
-import { Address } from 'viem';
+import { Address, http, createPublicClient } from 'viem';
 import { eip712WalletActions } from 'viem/zksync'
 import { zkSync } from 'viem/chains'
 import { useWalletClient } from 'wagmi';
+import Link from 'next/link';
 
 const TOKEN_TO_SEND = "0xBBeB516fb02a01611cBBE0453Fe3c580D7281011" //WBTC
 const ETH_AMOUNT = 1000n; // amount of wei to send
-const API_URL = 'https://api.zyfi.org/api/erc20_paymaster/v1'
+const API_URL = 'https://api.zyfi.org/api/erc20_paymaster/v1' // Zyfi's API for crafting paymaster transactions
+
+// create a custom public Client to send raw transactions to the provided RPC,
+// instead of sending trhough the users wallet, as it is not supported on mobile
+const RPC_URL = 'https://mainnet.era.zksync.io'
+const transport = http(RPC_URL)
+const customClient = createPublicClient({
+  chain: zkSync,
+  transport
+})
 
 export default function EthPage() {
 
   const [txHash, setTxHash] = useState<string>(''); // hash of the transaction
   const [toAddress, setToAddress] = useState<Address>('0x'); // address to send ETH to
-  const { data: walletClient } = useWalletClient();
+  const { data: walletClient } = useWalletClient(); // use wallet connected through web3modal
 
   // Checks if an address is a valid Ethereum address
   function isValidEthereumAddress(toAddress: Address) {
@@ -21,7 +31,10 @@ export default function EthPage() {
   }
 
   const clickSendTransaction = async () => {
+    //update display
     setTxHash('')
+
+    //checks
     if (!walletClient) {
       alert('Please connect your wallet');
       return;
@@ -30,8 +43,10 @@ export default function EthPage() {
       alert('Invalid Ethereum address, please enter a valid Ethereum address.');
       return;
     }
+
     const address = walletClient.account.address
 
+    // tx request for Zyfi's API
     const txRequest = {
       "feeTokenAddress": TOKEN_TO_SEND,
       "isTestnet": false,
@@ -43,51 +58,37 @@ export default function EthPage() {
       }
     }
 
-    const res = await submitTxDataToAPI(API_URL, txRequest)
-    console.log('res', res)
+    // paymaster data received from Zyfi's API
+    const pmData = await submitTxDataToAPI(API_URL, txRequest)
 
-    const paymaster = res.txData.customData.paymasterParams.paymaster
-    const paymasterInput = res.txData.customData.paymasterParams.paymasterInput
 
     // extend wallet client to support paymaster transactions
     const wClient = walletClient.extend(eip712WalletActions())
 
-    const txPayload = {
+    //!!! Workaround to make paymaster transactions work on mobile
+    const txReq = await wClient.prepareTransactionRequest({
       account: address,
       to: toAddress,
       value: ETH_AMOUNT,
       chain: zkSync,
-      gas: BigInt(res.gasLimit),
-      gasPerPubdata: BigInt(res.txData.customData.gasPerPubdata),
-      maxFeePerGas: BigInt(res.txData.maxFeePerGas),
+      gas: BigInt(pmData.gasLimit),
+      gasPerPubdata: BigInt(pmData.txData.customData.gasPerPubdata),
+      maxFeePerGas: BigInt(pmData.txData.maxFeePerGas),
       maxPriorityFeePerGas: 0n,
-      data: res.txData.data,
-      paymaster,
-      paymasterInput,
-    }
-    console.log('tranasaction payload: ', txPayload)
+      data: pmData.txData.data,
+      paymaster: pmData.txData.customData.paymasterParams.paymaster,
+      paymasterInput: pmData.txData.customData.paymasterParams.paymasterInput,
+    })
 
-    try {
-      const hash = await wClient.sendTransaction(txPayload)
-      setTxHash(hash)
-      console.log('hash:', hash)
-      console.log('check it on zksync explorer:', `https://explorer.zksync.io/tx/${hash}`)
-    }
-    catch (e) {
-      console.log('error', e)
-    }
+    // const hash = await wClient.sendTransaction(txReq)
+    const signature = await wClient.signTransaction(txReq)
+    const hash = await customClient.sendRawTransaction({ serializedTransaction: signature })
+
+    // update display
+    setTxHash(hash)
   }
 
-  const clickSignMessage = async () => {
-    if (!walletClient) {
-      alert('Please connect your wallet');
-      return;
-    }
-
-    const signature = await walletClient.signMessage({ message: "hello" })
-    console.log('signature of "hello" : ', signature)
-  }
-
+  // page content
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-800 text-gray-300">
       <ConnectButton />
@@ -105,21 +106,19 @@ export default function EthPage() {
         <button onClick={clickSendTransaction} disabled={!isValidEthereumAddress(toAddress)} className='bg-blue-500 p-4 rounded-full disabled:opacity-50 disabled:bg-gray-500'>Send Transaction</button>
         <p className='text-sm'>Clicking on &quot;Send Transaction&quot; should send {Number(ETH_AMOUNT)} wei of ETH to the selected address while paying gas in WBTC (0xBBeB516fb02a01611cBBE0453Fe3c580D7281011)</p>
         <p className='text-orange-400'> transaction hash will show if successfull : </p>
-        <p className='text-orange-400'>{txHash ? txHash : 'no transaction sent'}</p>
-      </div>
-      <div className='flex-col justify-center text-center item-center'>
-        <button onClick={clickSignMessage} disabled={!walletClient} className='bg-blue-500 p-4 rounded-full disabled:opacity-50 disabled:bg-gray-500'>Sign Message</button>
-        <p className='text-sm'>button to verify simple signatures work</p>
+        {txHash ? (
+          <Link href={`https://explorer.zksync.io/tx/${txHash}`} className="underline text-green-500" target='_blank' rel="noopener noreferrer">
+            https://explorer.zksync.io/tx/${txHash}
+          </Link>
+        ) : (
+          <p className='text-orange-400'>No transaction sent</p>
+        )}
       </div>
     </main>
   );
 }
 
-function ConnectButton() {
-  return <w3m-button />
-}
-
-// calls Zyfi's api annd returns the data that should be added to transactions that use the paymaster
+// calls Zyfi's api and returns the data that should be added to transactions that use the paymaster
 async function submitTxDataToAPI(url: string, data: any) {
   let result;
   try {
@@ -141,4 +140,9 @@ async function submitTxDataToAPI(url: string, data: any) {
     console.error('There has been a problem with your fetch operation:', error);
   }
   return result
+}
+
+// web3modal button to connect wallet
+function ConnectButton() {
+  return <w3m-button />
 }
